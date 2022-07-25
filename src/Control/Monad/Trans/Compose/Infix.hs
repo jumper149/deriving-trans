45a26764b0b321b1afb48f9e18fd53c544263ab0
Felix Springer <felix.springer@possehl-analytics.com>
2022-07-25 11:11:13 +0200

    Improve haddock comments in `Control.Monad.Trans.Compose.Infix`.

--- src/Control/Monad/Trans/Compose/Infix.hs
+++ src/Control/Monad/Trans/Compose/Infix.hs
@@ -1,18 +1,14 @@
-module Control.Monad.Trans.Compose.Infix (
-  type (.|>),
-  (..>),
-  (./>),
-) where
+module Control.Monad.Trans.Compose.Infix where
 
 import Control.Monad.Trans.Compose
 import Control.Monad.Trans.Control
 
--- | A type-level infix operator for `ComposeT`.
+-- | A flipped type-level infix operator for 'ComposeT'.
 type (.|>) t2 t1 = ComposeT t1 t2
 
 infixl 1 .|>
 
--- | An infix operator for `runComposeT`
+-- | A 'flip'ped infix operator for 'runComposeT'.
 (./>) :: (forall a. t2 m a -> m (StT t2 a)) -- ^ run @t2@
       -> (forall a. t1 (t2 m) a -> t2 m (StT t1 a)) -- ^ run @t1@
       -> (forall a. (t2 .|> t1) m a -> m (StT t2 (StT t1 a)))
@@ -20,7 +16,7 @@ infixl 1 .|>
 
 infixl 1 ./>
 
--- | An infix operator for `runComposeT'`
+-- | A 'flip'ped infix operator for 'runComposeT''.
 (..>) :: (t2 m a -> m a) -- ^ run @t2@
       -> (t1 (t2 m) a -> t2 m a) -- ^ run @t1@
       -> ((t2 .|> t1) m a -> m a)
