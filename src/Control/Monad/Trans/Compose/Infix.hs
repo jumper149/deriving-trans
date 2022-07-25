5c51c242b08d8317aabf5cea9360c1c338499081
Felix Springer <felix.springer@possehl-analytics.com>
2022-07-25 10:38:49 +0200

    Add module `Control.Monad.Trans.Compose.Infix`.

---
+++ src/Control/Monad/Trans/Compose/Infix.hs
@@ -0,0 +1,29 @@
+module Control.Monad.Trans.Compose.Infix (
+  type (.|>),
+  (..>),
+  (./>),
+) where
+
+import Control.Monad.Trans.Compose
+import Control.Monad.Trans.Control
+
+-- A type-level infix operator for `ComposeT`.
+type (.|>) = ComposeT
+
+infixr 1 .|>
+
+-- An infix operator for `runComposeT`
+(./>) :: (forall a. t1 (t2 m) a -> t2 m (StT t1 a)) -- ^ run @t1@
+      -> (forall a. t2 m a -> m (StT t2 a)) -- ^ run @t2@
+      -> (forall a. (t1 .|> t2) m a -> m (StT t2 (StT t1 a)))
+(./>) = runComposeT
+
+infixr 1 ./>
+
+-- An infix operator for `runComposeT'`
+(..>) :: (t1 (t2 m) a -> t2 m a) -- ^ run @t1@
+      -> (t2 m a -> m a) -- ^ run @t2@
+      -> ((t1 .|> t2) m a -> m a)
+(..>) = runComposeT'
+
+infixr 1 ..>
