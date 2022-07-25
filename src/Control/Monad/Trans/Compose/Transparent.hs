ffcfaa65bc71194c97f028fe83cc02ed3abc2655
Felix Springer <felix.springer@possehl-analytics.com>
2022-07-25 12:46:23 +0200

    Export everything from `Control.Monad.Trans.Compose.TransparentT`.

--- src/Control/Monad/Trans/Compose/Transparent.hs
+++ src/Control/Monad/Trans/Compose/Transparent.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE UndecidableInstances #-}
 
-module Control.Monad.Trans.Compose.Transparent (
-  TransparentT,
-  runTransparentT,
-) where
+module Control.Monad.Trans.Compose.Transparent where
 
 import Control.Monad.Trans.Class
 import Control.Monad.Trans.Control
@@ -12,12 +9,19 @@ import Control.Monad.Trans.Elevator
 import Control.Monad.Trans.Identity
 
 -- | A monad transformer, that passes through all instances via 'Elevator'.
+--
+-- This cannot be defined as a newtype, because we want all the instances, that are defined for
+-- 'Elevator' to work for 'TransparentT'.
 type TransparentT = Elevator NoT
 
 runTransparentT :: TransparentT m a -> m a
 runTransparentT = runIdentityT . runNoT . descend
 
 -- | A newtype wrapper around 'IdentityT'.
+--
+-- This is used in 'TransparentT' to encourage the use of 'runTransparentT'.
 newtype NoT m a = MkNoT {runNoT :: IdentityT m a}
   deriving newtype (Functor, Applicative, Monad)
   deriving newtype (MonadTrans, MonadTransControl, MonadTransControlIdentity)
+
+{-# WARNING NoT, MkNoT, runNoT "This is an implementation detail of 'TransparentT'." #-}
