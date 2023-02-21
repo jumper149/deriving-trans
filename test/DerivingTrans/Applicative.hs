f9eee081b72bf564b2e6e0f0078e8526d5ae0ae0
Felix Springer <felixspringer149@gmail.com>
2023-02-21 17:33:30 +0100

    Add laws for `Applicative`.

--- test/DerivingTrans/Applicative.hs
+++ test/DerivingTrans/Applicative.hs
@@ -9,6 +9,9 @@ tests =
   testGroup
     "Applicative"
     [ testProperty "Identity" $ lawIdentity (T.runExcept @()) (pure ())
+    , testProperty "Composition" $ lawComposition (T.runExcept @()) (pure ()) (pure id) (pure id)
+    , testProperty "Homomorphism" $ lawHomomorphism (T.runExcept @()) () id
+    , testProperty "Interchange" $ lawInterchange (T.runExcept @()) () (pure id)
     ]
 
 lawIdentity ::
@@ -18,3 +21,31 @@ lawIdentity ::
   f a ->
   Property
 lawIdentity run x = run (pure id <*> x) === run x
+
+lawComposition ::
+  forall f y a b c.
+  (Applicative f, Eq y, Show y) =>
+  (f c -> y) ->
+  f a ->
+  (f (a -> b)) ->
+  (f (b -> c)) ->
+  Property
+lawComposition run x g f = run (pure (.) <*> f <*> g <*> x) === run (f <*> (g <*> x))
+
+lawHomomorphism ::
+  forall f y a b.
+  (Applicative f, Eq y, Show y) =>
+  (f b -> y) ->
+  a ->
+  (a -> b) ->
+  Property
+lawHomomorphism run x f = run (pure f <*> pure x) === run (pure (f x))
+
+lawInterchange ::
+  forall f y a b.
+  (Applicative f, Eq y, Show y) =>
+  (f b -> y) ->
+  a ->
+  (f (a -> b)) ->
+  Property
+lawInterchange run x f = run (f <*> pure x) === run (pure ($ x) <*> f)
